518e4c2be4562ca3d3105f26bb9cd42791105906
anujkumarparashar <anujparashar.iimt@gmail.com>
2026-02-28 20:20:19 +0530

    fixes

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: {
-        unoptimized: true,
-    },
     eslint: {
         ignoreDuringBuilds: true,
     },
+    images: {
+        unoptimized: true,
+    },
+    basePath: '/portfolio',
 };
 
 export default nextConfig;
